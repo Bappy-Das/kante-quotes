a07b956feeb9630b6ce71647c3893db53df7d759
Bappy-Das <bappydas.bd51@gmail.com>
2021-08-29 20:52:41 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const loadQuotes = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuotes(data))
+}
+loadQuotes();
+const displayQuotes = quote => {
+    // console.log(quote);
+    const quoteElement = document.getElementById('quote');
+    const quotes = quote.quote;
+    quoteElement.innerHTML = `“ ${quotes} ”`;
+}
